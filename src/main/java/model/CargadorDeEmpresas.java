1d465158448693684a5bfe1427e9c1657968e85a
Gabriel Hernan Spisso <gabrielhspisso@gmail.com>
2017-04-27 12:38:15 -0300

    Agregue el archivo de lectura
    El archivo que lee esta dentro de la carpeta model.
    Otra forma de poner la ruta de lectura del archivo es poniendo todo el
    path por ejemplo:
    "/Users/Gabriel/Documents/archivoEmpresas.txt"

--- src/main/java/model/CargadorDeEmpresas.java
+++ src/main/java/model/CargadorDeEmpresas.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class CargadorDeEmpresas {
 	
 	public static List<Empresa> obtenerCuentasEmpresas(){
-		return ArchivoMocking.leerArchivo("/Users/Gabriel/Documents/archivoEmpresas.txt");
+		return ArchivoMocking.leerArchivo("ArchivoEmpresas.txt");
 	}
 	
 }
