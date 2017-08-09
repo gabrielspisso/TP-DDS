711a0fa178ba5bd8371f6285d9bbd88748ec0e70
SantiagoParedes97 <sparedes@est.frba.utn.edu.ar>
2017-08-08 23:38:35 -0300

    Cambios de nombre para que no rompa y subo el nuevo diagrama de clases

---
+++ src/main/java/model/Arbol/Operaciones/Division.java
@@ -0,0 +1,18 @@
+package model.Arbol.Operaciones;
+
+import java.util.List;
+
+import model.Cuenta;
+import model.Indicador;
+
+public class Division extends Operacion{
+
+	public Division(NODO izquierda, NODO derecha) {
+		super("/", izquierda, derecha);
+	}
+	@Override
+	public double calcularValor(List<Cuenta> listaDeCuentas, List<Indicador> listaDeIndicadores) {
+		return izquierda.calcularValor(listaDeCuentas, listaDeIndicadores) /
+				derecha.calcularValor(listaDeCuentas, listaDeIndicadores);
+	}
+}
