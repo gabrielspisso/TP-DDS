431f93883cea0f464271341c8e1842273a9bc94a
SantiagoParedes97 <sparedes@est.frba.utn.edu.ar>
2017-08-24 23:47:59 -0300

    Olvidaddas

--- src/main/java/condicionesYMetodologias/CondicionConAño.java
+++ src/main/java/condicionesYMetodologias/CondicionConAño.java
@@ -2,6 +2,7 @@ package condicionesYMetodologias;
 import java.util.List;
 
 import Calculos.criterioDeAceptacionDeCondicion;
+import Excepciones.IdentificadorInexistente;
 import model.Balance;
 import model.Empresa;
 import model.Indicador;
@@ -10,23 +11,27 @@ public class CondicionConAño extends Condicion{
 	double valorMinimo;
 	int cantidadDeAños;
 	public CondicionConAño(Indicador indicador, criterioDeAceptacionDeCondicion criterio, double valorMinimo,int cantidadDeAños){
-		super(indicador,criterio);
+		super(indicador,criterio,"");
 		this.valorMinimo = valorMinimo;
 		this.cantidadDeAños = cantidadDeAños;
 		
 	}
-	@Override
+	public CondicionConAño(ValoresParaEvaluar valores) {
+		super(valores);
+		this.valorMinimo = valores.getValorMinimo();
+		this.cantidadDeAños = valores.getCantidadDeAños();
+	}
+	
 	public boolean cumpleCondicion(Empresa empresa, Empresa empresa1){
 		if(empresa.getBalances().size() < cantidadDeAños){
 			return false;
 		}
 		List<Balance> listaDeBalances = empresa.getBalances().subList(0,cantidadDeAños);
-		return listaDeBalances.stream().allMatch(balance->criterio.cumpleCriterioDeAceptacionDeCondicion(this.valorMinimo,this.indicador.calcularValor(balance.getCuentas())));	
-	}
-	@Override
-	public String toString(){
-		return indicador.toString()+" es "+criterio.toString()+" a "+ valorMinimo + " durante "+cantidadDeAños;
-		
-		//Intente que no repita lo de indicador to string y que se ocupe la clase padre, pero a java no le importo y me tomaba la del padre.
+		try{
+			return listaDeBalances.stream().allMatch(balance->criterio.cumpleCriterioDeAceptacionDeCondicion(this.valorMinimo,this.indicador.calcularValor(balance.getCuentas())));				
+		}
+		catch(IdentificadorInexistente ex){
+			return false;
+		}
 	}
 }
