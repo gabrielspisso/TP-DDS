370f162385b43cc36ddbd33c4577fba9b8382897
SantiagoParedes97 <sparedes@est.frba.utn.edu.ar>
2017-11-17 12:45:07 -0300

    test agregado
    test

--- src/test/java/testEntrega6.java
+++ src/test/java/testEntrega6.java
@@ -12,13 +12,12 @@ import model.repositorios.RepositorioDeEmpresas;
 public class testEntrega6 {
 
 	@Test
-	public void test() {
+	public void testearQueSeGuardaElPrecalculo() {
 		new LeerEmpresasProgramado("archivoEmpresas.txt").run();;
 		
 		Indicador indicador = IOs.listaDeIndicadoresMockeada().get(0);
-		System.out.println("Entre al coso ");
-		Balance balance = RepositorioDeEmpresas.traerEmpresasDeLaDB().get(0).getBalances().get(0);
-		//Balance balance =CargadorDeEmpresas.obtenerCuentasEmpresasHardcodeada().get(0).getBalances().get(0);
+		//Balance balance = RepositorioDeEmpresas.traerEmpresasDeLaDB().get(0).getBalances().get(0);
+		Balance balance =CargadorDeEmpresas.obtenerCuentasEmpresasHardcodeada().get(0).getBalances().get(0);
 		indicador.evaluarBalance(balance);
 		assertTrue(balance.estaPrecalculadoElIndicador(indicador));
 		//new LeerEmpresasProgramado("archivoEmpresas.txt").run();;
@@ -26,10 +25,14 @@ public class testEntrega6 {
 		
 	}
 	@Test
-	public void test2() {
+	public void testearQueSeGuardaElValorCorrecto() {
 		new LeerEmpresasProgramado("archivoEmpresas.txt").run();;
 		
 		Indicador indicador = IOs.listaDeIndicadoresMockeada().get(0);
+		//Balance balance = RepositorioDeEmpresas.traerEmpresasDeLaDB().get(0).getBalances().get(0);
+		Balance balance =CargadorDeEmpresas.obtenerCuentasEmpresasHardcodeada().get(0).getBalances().get(0);
+		String valor = indicador.evaluarBalance(balance).getResultado();
+		assertEquals(valor,balance.buscarEnListaDeResultados(indicador).toString());
 		//new LeerEmpresasProgramado("archivoEmpresas.txt").run();;
 		
 		
