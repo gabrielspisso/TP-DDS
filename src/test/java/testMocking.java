c4031eb41dca977f39d2612121874f27c5154a51
gabrielspisso <gabrielspisso@outlook.com>
2017-11-17 01:10:02 -0300

    pusheo una linea
    con un test que mockea un repo

--- src/test/java/testMocking.java
+++ src/test/java/testMocking.java
@@ -1,12 +1,18 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.mockito.Mockito.*;
 import org.mockito.*;
 import org.mockito.Mock;
 
 import org.mockito.Matchers.*;
+import model.Empresa;
+import model.CargadorDeEmpresas;
 import model.Indicador;
+import model.repositorios.Repositorio;
+import model.repositorios.RepositorioDeEmpresas;
 
 public class testMocking {
 
@@ -15,6 +21,12 @@ public class testMocking {
 		
 		Indicador ind = Mockito.mock(Indicador.class);
 		Mockito.when(ind.getNombre()).thenReturn("hola");
+		List<Indicador> as;
+		RepositorioDeEmpresas repo = Mockito.mock(RepositorioDeEmpresas.class);
+		
+		
+		Mockito.when(repo.traerEmpresasDeLaDB()).thenReturn(CargadorDeEmpresas.obtenerCuentasEmpresasHardcodeada());
+		
 		//Assert.assertEquals(servicioB.multiplicar(2, 3),6);
 		
 		fail("Not yet implemented");
